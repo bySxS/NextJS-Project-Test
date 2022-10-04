3ca98f58df1613ffb61533be5b8570a967e19da9
bySxS <88063532+bySxS@users.noreply.github.com>
2022-10-04 15:12:19 +0300

    fix type post

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,20 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-
-type IPost = {
-  id: number
-  title: string
-  body: string
-}
+import { IPosts } from '../../../ts-types/posts.interface'
 
 const getPostById = async (id: number) => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/db.json`)
-  const json: { posts: IPost[] } = await data.json()
+  const json: { posts: IPosts[] } = await data.json()
   return json.posts.filter((p) => p.id === id)[0]
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<IPost>
+  res: NextApiResponse<IPosts>
 ) {
   const post = await getPostById(+(req.query.id ?? 0))
   res.status(200).json(post)
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,20 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type IPost = {
-  id: number
-  title: string
-  body: string
-}
+import { IPosts } from '../../../ts-types/posts.interface'
 
 const getPosts = async () => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/db.json`)
-  const json: { posts: IPost[] } = await data.json()
+  const json: { posts: IPosts[] } = await data.json()
   return json.posts
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<IPost[]>
+  res: NextApiResponse<IPosts[]>
 ) {
   const posts = await getPosts()
   res.status(200).json(posts)
